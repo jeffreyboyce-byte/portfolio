ea32d66e44292860bbf8b32b8300fb844154a6d5
jeffreyboyce-byte <jeffreyboyce@gmail.com>
2026-08-25 13:36:48 -0700

    Add files via upload

---
+++ old/projects/astro-checkout-process/index.php
@@ -0,0 +1,127 @@
+<?php include ('../header.php'); ?> 	
+
+<!--GLOBAL STYLESHEET-->
+<link href="../../global.css" rel="stylesheet" type="text/css" />
+    
+    <div id="gray-hero" style="padding:0px;">
+    		<div id="project-logo"><img src="images/astro-logo.png" class="project-logo" /></div>
+    		<div id="project-header-title"><img src="images/header.png" class="project-header-title" /></div>
+        	<div id="project-description" style="width:650px;">A critical point in the online shopping funnel, it was important to ensure a simple and ubiquitous experience for users across any device they may use.</div>
+    		<div style="margin:auto;height:480px;width:880px;background:url(images/hero.png) no-repeat 0px 30px;"></div>
+    </div>
+    
+    <!--CONTENT-->
+    <div id="content">
+    	
+        <div id="research-section" style="border:0px solid red;">
+        
+        	<div id="section-icon" style="background:url(../images/research-icon.png) no-repeat 50% 0px;"></div>
+            
+            <div id="section-title">Research</div>
+            
+            <div id="section-text-middle" style="text-align:justify;">At the time of this project, the company was conducting a replatform of its ecommerce site - a huge undertaking considering the potential ramifications on transactions. All aspects of the site were evaluated and analyzed including the critical checkout process. I was tasked with evaluating the out-of-the-box checkout process of the new platform and enhancing it for our audience.</div>
+            
+            <div style="margin:0 auto;padding:60px 0px 60px 0px;height:790px;width:880px;border-top:1px solid black;border-bottom:1px solid black;text-align:center;">
+            	<div id="section-subtitle-large" style="margin:0 auto;display:block;padding:0px 0px 15px 0px;">Market research</div>
+                <div id="section-text-middle" style="border:0px solid red;padding:0px 0px 0px 0px;width:550px;font-weight:normal;text-align:justify;">My focus in the market was selective towards the most successful checkout systems and included those in the electronic hardware space. I took time analyzing these effective market platforms by evaluating their best practices and also taking note of where they fell short. I converged all of this information to provide proper intel to begin setting up the project.</div>
+            	<div style="margin:auto;height:616px;width:880px;background:url(images/diagram-1.png) no-repeat 0px 60px;"></div>
+            </div>
+            
+            <div style="margin:0 auto;padding:60px 0px 60px 0px;height:480px;width:880px;background:url(images/evaluation.png) no-repeat 0px 75px;border-top:0px solid black;border-bottom:0px solid black;text-align:justify;">
+            	<div id="section-subtitle-large" style="display:block;padding:0px 0px 15px 0px;">Replatform<br />evaluation</div>
+                <div id="section-text" style="float:left;border:0px solid red;padding:0px 0px 0px 0px;width:329px;font-weight:normal;">In order to provide the development team a framework to begin, I quickly evaluated the default checkout process of the new platform and presented a thorough analysis of what needed to be changed to cater to Astro fans.</div>
+            </div>
+            
+            <div id="section-subtitle" style="margin:auto;width:880px;padding:0px 0px 60px 0px;text-align:center;">This initial markup was enough to get the developers started. It provided me the proper time to perform extensive research on effective checkout systems in the market and define a setup that was optimal for our our own process.</div>
+            
+            <div id="section-subtitle" style="margin:0 auto;padding:15px 0px 15px 0px;width:880px;border-top:2px solid black;border-bottom:2px solid black;text-align:center;">Goals</div>
+        	<div id="section-text" style="padding:30px 0px 0px 0px;color:black;border:0px solid red;">
+            	<ul class="red-squares" style="padding:0px 0px 0px 0px;width:350px;">
+              		<li class="red-squares"><b>Analyze and enhance default checkout system for new ecommerce platform</b></li><br />
+              		<li class="red-squares"><b>Analyze successful marketplace checkout systems</b></li><br />
+              		<li class="red-squares"><b>Optimize the experience for desktop, tablet, and mobile devices</b></li><br />
+              		<li class="red-squares"><b>Design for localization in current European and Asian markets</b></li>
+              	</ul>
+            </div>
+            
+        </div>
+        
+        <div style="clear:both;height:2480px;"></div>
+        
+        <div id="brainstorm-section" style="height:3551px;">
+        
+        	<div id="section-icon" style="background:url(../images/brainstorm-icon.png) no-repeat 50% 0px;"></div>
+            <div id="section-title">Brainstorm</div>
+            <div id="section-text-middle" style="text-align:justify;">After a thorough research across multiple checkout processes in the marketplace that are known to have a high conversion rate, I proceed to outline a general flow that broke down all of the phases into a simple experience.</div>
+        	
+             <div style="margin:0 auto;padding:0px 0px 60px 0px;height:1134px;width:880px;border-top:0px solid black;border-bottom:0px solid black;text-align:center;">
+            	<div style="margin:auto;width:673px;height:1134px;background:url(images/diagram-2.png) no-repeat 0px 0px;"></div>
+           	 </div>
+             <div style="margin:0 auto;padding:60px 0px 60px 0px;height:650px;width:880px;border-top:1px solid black;border-bottom:0px solid black;text-align:center;">
+            	<div id="section-subtitle-large" style="display:block;padding:0px 0px 15px 0px;">Defining the details</div>
+                <div id="section-text-middle" style="border:0px solid red;padding:0px 0px 60px 0px;width:600px;">With the general flow in place, I began documenting all of the features and details that were needed throughout the checkout process. Since there was so much information needed from the user, it was important to segment everything into their appropriate sections to make it easy for the user to complete.</div>
+            	<div style="margin:auto;width:696px;height:409px;background:url(images/laptop.png) no-repeat 0px 0px;"></div>
+                <div id="section-text-middle" style="border:0px solid red;padding:60px 0px 60px 0px;">With the blueprint of all the pages and features that needed to be implemented, I was ready to begin developing the wireframes.</div>
+            </div>
+            
+        </div>
+        
+        <div style="clear:both;height:2450px;"></div>
+            
+        <div id="wireframe-section">
+            <div id="section-icon" style="background:url(../images/wireframe-icon.png) no-repeat 50% 0px;"></div>
+            <div id="section-title">Quick iterative wireframes</div>
+            <div id="section-text-middle" style="width:700px;text-align:center;">The checkout process is a critical point in the shopping funnel. A the same time, a big challenge is that it requires a heavy input from users which can be daunting and tedious. It was important to think through designs that made this part easy and fluid for users.</div>
+            <div style="margin:0 auto;width:880px;height:295px;background:url(images/wireframes.png) no-repeat 0px 0px;"></div>
+        	<div id="section-text-middle" style="width:700px;padding:60px 0px 0px 0px;text-align:center;">In addition to these wireframes, I also produced ones that were optimized for tablet and mobile devices. Since these spaces were dealing with smaller real estate sizes, I had to be more conscious of the information that was displayed.</div>
+        </div>
+        
+        <div style="clear:both;height:888px;"></div>
+        
+        <div id="visuals-section">
+        	<div id="section-icon" style="background:url(images/visual-icon.png) no-repeat 50% 0px;"></div>
+            <div id="section-title">Visual design</div>
+            <div id="section-text-middle" style="width:750px;text-align:center;">The language and visual design were delivered to match the Astro Gaming brand and UI style guide.</div>
+            <div style="margin:0 auto;width:950px;">
+             	<div style="margin:0 auto;width:880px;height:1345px;background:url(images/visuals-1.png) no-repeat 0px 0px;">
+            		<div id="section-subtitle" style="width:400px;margin:0 auto;padding:1380px 0px 0px 0px;text-align:center;">Approachable interface</div>
+                    <div id="section-subtitle-gray" style="clear:both;width:550px;margin:0 auto;text-align:center;">In order to make these forms less daunting, I was generous with padding and enlarged the UI elements.</div>
+            	</div>
+                <div style="clear:both;height:300px;"></div>
+                <div style="margin:0 auto;width:920px;height:710px;background:url(images/visuals-2.png) no-repeat 0px 0px;padding:0px 0px 0px 20px;">
+            		<div id="section-subtitle" style="float:left;padding:510px 0px 0px 0px;width:280px;">Timely information</div>
+                    <div id="section-subtitle-gray" style="clear:both;float:left;width:280px;">Only relevant requests are presented through the user's choices.</div>
+                    <div id="section-text">
+                    	<div id="section-text" style="float:right;margin:180px 90px 0px 15px;width:360px;font-size:12px;text-align:center;">
+                        <font color="black"><b>Credit card information is only requested if a user selected that as their method of payment.</b></font>
+                    	</div>
+                	</div>
+            	</div>
+                <div style="clear:both;height:240px;"></div>
+                <div style="margin:0 auto;width:950px;height:639px;background:url(images/visuals-3.png) no-repeat 3px 0px;">
+            		<div id="section-subtitle" style="margin:auto;width:550px;padding:445px 0px 0px 0px;text-align:center;">Designed for localization</div>
+                    <div id="section-subtitle-gray" style="clear:both;margin:auto;width:800px;text-align:center;">With some Asian and European markets already established, it was important to design accordingly to accommodate other languages. Text field titles on top provided ample room for any language. Headers were also important to designate field properties since text field values disappear on click.</div>
+            	</div>
+                <div style="clear:both;height:240px;"></div>
+                <div style="margin:0 auto;width:950px;height:500px;background:url(images/visuals-4.png) no-repeat 0px 0px;">
+            		<div id="section-subtitle" style="float:right;width:260px;margin:0px 0px 0px 0px;padding:120px 0px 0px 0px;">Responsively designed</div>
+                    <div id="section-subtitle-gray" style="clear:both;float:right;width:260px;margin:0px 0px 0px 0px;">With ecommerce transactions growing beyond the desktop, it was important to design for any device that may be used to purchase Astro Gaming products.</div>
+            	</div>
+                <div style="clear:both;height:240px;"></div>
+                <div style="margin:0 auto;width:880px;height:1323px;background:url(images/visuals-5.png) no-repeat 180px 0px;">
+            		<div id="section-subtitle" style="float:left;width:250px;margin:0px 0px 0px 0px;padding:90px 0px 0px 0px;">Mobile ready</div>
+                    <div id="section-subtitle-gray" style="clear:both;float:left;width:280px;margin:0px 0px 0px 0px;">For those who prefer to shop on their phones, the site is optimized to fit and function easily on the smaller real estate.</div>
+            	</div>
+            </div>
+        </div>
+        
+        <div style="clear:both;height:6050px;width:500px;"></div>
+        
+        <div id="fin">Fin<br /><a class="red-btn" href="http://makilaban.com/work.php">VIEW ALL WORK</a></div>
+        
+        <div id="footer-spacer"></div>
+        
+    </div>
+    <!--CONTENT-->
+    
+<?php include ('../../footer.php'); ?> 	
